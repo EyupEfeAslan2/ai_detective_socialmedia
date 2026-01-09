c938121b36f3b19c44897b01c152c15eaea67666
EyupEfeAslan2 <aslaneyupefe2@gmail.com>
2026-01-09 15:44:45 +0300

    Güvenlik ayarları ve yerel harici veri çekimine izin verildi.

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,10 @@ import os
 app = FastAPI()
 
 # --- CORS ---
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=["*"] diyerek tüm sitelere izin veriyoruz (En garanti yöntem)
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
