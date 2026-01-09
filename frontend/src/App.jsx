349d09b438958bbe0387d51cae61ae14314878e4
EyupEfeAslan2 <aslaneyupefe2@gmail.com>
2026-01-09 15:40:05 +0300

    Canlıya alma backend render.com

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ const analyzeAccount = async (e) => {
 
     try {
       // 1. Backend'e İstek Gönder (Fetch API)
-      const response = await fetch('http://127.0.0.1:8000/analyze', {
+      const response = await fetch('https://ai-detective-backend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
